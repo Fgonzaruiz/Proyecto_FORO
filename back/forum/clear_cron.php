2a3d4a830052b6cfa3801eeb784efcbcc543e333
Francisco José González <fgonzaruiz@gmail.com>
2026-05-21 14:46:50 +0200

    Fix bootstrap path in clear_cron

--- back/forum/clear_cron.php
+++ back/forum/clear_cron.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/game/bootstrap.php';
 global $db;
 $prefix = TABLE_PREFIX;
 $db->write_query("UPDATE {$prefix}game_personajes SET cronologia_json = ''");
