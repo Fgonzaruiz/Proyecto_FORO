e627984a2d9e72ab80a07c7e8573100b6baeafb4
Francisco José González <fgonzaruiz@gmail.com>
2026-05-21 14:45:16 +0200

    Move clear_cron to correct web root

--- back/forum/clear_cron.php
+++ back/forum/clear_cron.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/back/forum/bootstrap.php';
+require_once __DIR__ . '/bootstrap.php';
 global $db;
 $prefix = TABLE_PREFIX;
 $db->write_query("UPDATE {$prefix}game_personajes SET cronologia_json = ''");
